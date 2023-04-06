a43228da8cd67dc8a90600959280e642059f7b30
carschandler <92899389+carschandler@users.noreply.github.com>
2023-04-06 12:15:47 +0000

    On hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -35,6 +35,20 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+        let team_1: &mut Team = match scores.get(&team_1_name) {
+            Some(mut t) => {&mut t}
+            None => {
+                let mut t = Team {
+                    name: team_1_name,
+                    goals_scored: 0,
+                    goals_conceded: 0
+                };
+                scores.insert(&team_1_name, t);
+                
+            }
+        };
+        team_1.goals_scored += team_1_score;
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
