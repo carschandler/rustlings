cfc0e3441ab4ad2c0c1da48c1bb059aa1fa561f5
Cars Chandler <carschandler7@gmail.com>
2023-09-04 00:57:18 -0500

    Complete from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let tokens: Vec<&str> = s.split(',').collect();
+        if tokens.len() != 2 { return Person::default() };
+        let name = tokens[0].to_string();
+        if name.is_empty() {
+            return Person::default();
+        }
+        let age = match tokens[1].parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => return Person::default()
+        };
+        return Person { name, age }
     }
 }
 
